25ef17a354a391bb4c4fd679e5dc46e785a7dca2
HerrBergenholtz <zbdv8800@gmail.com>
2024-05-30 23:32:55 +0200

    Inlämning

--- Medicinering/Form4.Designer.cs
+++ Medicinering/Form4.Designer.cs
@@ -42,9 +42,10 @@
             // 
             listView1.Columns.AddRange(new ColumnHeader[] { Id, Namn, Dos, Tider });
             listView1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            listView1.Location = new Point(13, 56);
+            listView1.Location = new Point(11, 42);
+            listView1.Margin = new Padding(3, 2, 3, 2);
             listView1.Name = "listView1";
-            listView1.Size = new Size(866, 316);
+            listView1.Size = new Size(758, 238);
             listView1.TabIndex = 0;
             listView1.UseCompatibleStateImageBehavior = false;
             listView1.View = View.Details;
@@ -67,25 +68,25 @@
             // Tider
             // 
             Tider.Text = "Tider";
-            Tider.Width = 340;
+            Tider.Width = 220;
             // 
             // textBox1
             // 
             textBox1.BorderStyle = BorderStyle.None;
             textBox1.Font = new Font("Segoe UI", 12F, FontStyle.Regular, GraphicsUnit.Point);
-            textBox1.Location = new Point(536, 92);
+            textBox1.Location = new Point(532, 69);
+            textBox1.Margin = new Padding(3, 2, 3, 2);
             textBox1.Multiline = true;
             textBox1.Name = "textBox1";
-            textBox1.Size = new Size(342, 277);
+            textBox1.Size = new Size(236, 208);
             textBox1.TabIndex = 1;
             // 
             // closeForm
             // 
             closeForm.Font = new Font("Segoe UI", 14F, FontStyle.Regular, GraphicsUnit.Point);
-            closeForm.Location = new Point(286, 390);
-            closeForm.Margin = new Padding(3, 4, 3, 4);
+            closeForm.Location = new Point(250, 292);
             closeForm.Name = "closeForm";
-            closeForm.Size = new Size(319, 47);
+            closeForm.Size = new Size(279, 35);
             closeForm.TabIndex = 3;
             closeForm.Text = "Stäng";
             closeForm.UseVisualStyleBackColor = true;
@@ -95,21 +96,22 @@
             // 
             label1.AutoSize = true;
             label1.Font = new Font("Segoe UI", 15F, FontStyle.Regular, GraphicsUnit.Point);
-            label1.Location = new Point(369, 9);
+            label1.Location = new Point(323, 7);
             label1.Name = "label1";
-            label1.Size = new Size(146, 35);
+            label1.Size = new Size(115, 28);
             label1.TabIndex = 4;
             label1.Text = "Information";
             // 
             // Form4
             // 
-            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(892, 450);
+            ClientSize = new Size(780, 338);
             Controls.Add(label1);
             Controls.Add(closeForm);
             Controls.Add(textBox1);
             Controls.Add(listView1);
+            Margin = new Padding(3, 2, 3, 2);
             Name = "Form4";
             Text = "Form4";
             ResumeLayout(false);
